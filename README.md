24fc67510224c3d896e1baa4aff8c87cbe1fe239
Konrad Dryja <kdryja@gmail.com>
2018-04-06 18:21:28 +0100

    Control LEDs with a remote

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Arduino files to manage the TrustLens' LED system.
 # Pinout
 Pinout schematic for the board for the future reference. Used board is **Arduino Uno R3**
 ![Pinout](graphics/pinout.png)
+# Useful resources
+- [For calculating resistance of a resistor.](https://www.digikey.co.uk/en/resources/conversion-calculators/conversion-calculator-resistor-color-code-4-band)
--- TrustLens.ino
+++ TrustLens.ino
@@ -1,9 +1,19 @@
+#include <IRremote.h>
 #include <Arduino.h>
 /*
 Adafruit Arduino - Lesson 3. RGB LED
 */
 
 int ledPin = 7;
+int redPin = 2;
+int greenPin = 3;
+int bluePin = 4;
+
+int RECV_PIN = 11;
+bool state = false;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
 //uncomment this line if using a Common Anode LED
 //#define COMMON_ANODE
@@ -11,12 +21,47 @@ int ledPin = 7;
 void setup()
 {
   pinMode(ledPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  Serial.begin(9600);
+  irrecv.enableIRIn();
 }
 
 void loop()
 {
-  digitalWrite(ledPin, LOW);
-  delay(1000);
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
+  if (irrecv.decode(&results))
+  {
+    Serial.println(results.value, HEX);
+    state = !state;
+    delay(250);
+    irrecv.resume(); // Receive the next value
+  }
+  if (state)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  if (results.value == 0xFF30CF)
+  {
+    setColor(255, 0, 0);
+  }
+  if (results.value == 0xFF18E7)
+  {
+    setColor(0, 255, 0);
+  }
+  if (results.value == 0xFF7A85)
+  {
+    setColor(0, 0, 255);
+  }
+}
+
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
 }
