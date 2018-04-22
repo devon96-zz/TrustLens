c15e26d787145baf7e1abcd7ea57ecd27687fbc3
Konrad Dryja <kdryja@gmail.com>
2018-04-22 20:07:51 +0100

    Add full sequence and schematics

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Pinout schematic for the board for the future reference. Used board is **Arduino
 ![Pinout](graphics/pinout.png)
 # Useful resources
 - [For calculating resistance of a resistor.](https://www.digikey.co.uk/en/resources/conversion-calculators/conversion-calculator-resistor-color-code-4-band)
+
+# Room distribution
+- Alarm - Blue cable - Pin 11
+- Bathroom - Orange cable - Pin 10
+- Fridge - Green cable - Pin 9
+- Lounge - Purple cable - Pin 8
+- Kid's room - Yellow cable - Pin 7
+- Button - Pin 12 
--- TrustLens.ino
+++ TrustLens.ino
@@ -4,64 +4,78 @@
 Adafruit Arduino - Lesson 3. RGB LED
 */
 
-int ledPin = 7;
-int redPin = 2;
-int greenPin = 3;
-int bluePin = 4;
+int alarmPin = 11;
+int bathroomPin = 10;
+int fridgePin = 9;
+int loungePin = 8;
+int kidPin = 7;
 
-int RECV_PIN = 11;
-bool state = false;
-
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-
-//uncomment this line if using a Common Anode LED
-//#define COMMON_ANODE
+int buttonPin = 12;
+int prevPress = 0;
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  pinMode(redPin, OUTPUT);
+  pinMode(alarmPin, OUTPUT);
+  pinMode(bathroomPin, OUTPUT);
+  pinMode(fridgePin, OUTPUT);
+  pinMode(loungePin, OUTPUT);
+  pinMode(kidPin, OUTPUT);
+
   Serial.begin(9600);
-  irrecv.enableIRIn();
+  pinMode(buttonPin, INPUT);
+  digitalWrite(buttonPin, HIGH);
 }
 
 void loop()
 {
-  if (irrecv.decode(&results))
+  int val = digitalRead(buttonPin); // read the input pin
+  if (prevPress != val && val == 1) // User has pressed the button
   {
-    Serial.println(results.value, HEX);
-    state = !state;
-    delay(250);
-    irrecv.resume(); // Receive the next value
-  }
-  if (state)
-  {
-    digitalWrite(ledPin, HIGH);
-  }
-  else
-  {
-    digitalWrite(ledPin, LOW);
-  }
-  if (results.value == 0xFF30CF)
-  {
-    setColor(255, 0, 0);
-  }
-  if (results.value == 0xFF18E7)
-  {
-    setColor(0, 255, 0);
-  }
-  if (results.value == 0xFF7A85)
-  {
-    setColor(0, 0, 255);
-  }
-}
+    Serial.println("Buttom press changed!");
 
-void setColor(int red, int green, int blue)
-{
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);
+    digitalWrite(alarmPin, HIGH); // Alarm - ON
+    delay(1000);
+    digitalWrite(bathroomPin, HIGH); // Bathroom - ON
+    delay(1000);
+    digitalWrite(fridgePin, HIGH); // Fridge - ON
+    delay(1000);
+    digitalWrite(fridgePin, LOW); // Fridge - OFF
+    delay(1000);
+    digitalWrite(loungePin, HIGH); // Lounge - ON
+    delay(1000);
+    digitalWrite(fridgePin, HIGH); // Fridge - ON
+    delay(1000);
+    digitalWrite(fridgePin, LOW); // Fridge - OFF
+    delay(1000);
+    digitalWrite(alarmPin, LOW); // Alarm - OFF
+    delay(1000);
+    digitalWrite(bathroomPin, LOW); // Bathroom - OFF
+    delay(1000);
+    digitalWrite(kidPin, HIGH); // Kid's Room - ON
+    delay(1000);
+    digitalWrite(bathroomPin, HIGH); // Bathroom - ON
+    delay(1000);
+    digitalWrite(bathroomPin, LOW); // Bathroom - OFF
+    delay(1000);
+    digitalWrite(kidPin, LOW); // Kid's Room - OFF
+    delay(1000);
+    digitalWrite(fridgePin, HIGH); // Fridge - ON
+    delay(1000);
+    digitalWrite(fridgePin, LOW); // Fridge - OFF
+    delay(1000);
+    digitalWrite(bathroomPin, HIGH); // Bathroom - OFF
+    delay(1000);
+    digitalWrite(bathroomPin, LOW); // Bathroom - OFF
+    delay(1000);
+    digitalWrite(loungePin, LOW); // Lounge - OFF
+  }
+  if (val == 0)
+  {
+    digitalWrite(alarmPin, LOW);
+    digitalWrite(bathroomPin, LOW);
+    digitalWrite(fridgePin, LOW);
+    digitalWrite(loungePin, LOW);
+    digitalWrite(kidPin, LOW);
+  }
+  prevPress = val;
 }
